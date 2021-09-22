e6f44bb73a0f383cefd858aec76a4a5e104fe037
Keon <keonlee93@naver.com>
2021-09-22 18:56:32 +0900

    Add Auth Folder

---
+++ lib/auth/auth_bloc_provider.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AuthBlocProvider extends StatelessWidget {
+  const AuthBlocProvider({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ lib/auth/auth_view.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class AuthView extends StatefulWidget {
+  const AuthView({Key? key}) : super(key: key);
+
+  @override
+  _AuthViewState createState() => _AuthViewState();
+}
+
+class _AuthViewState extends State<AuthView> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
